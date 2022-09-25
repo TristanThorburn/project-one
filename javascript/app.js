af72762edca1b471f83a03680bef4319fe57da08
Tristan <tristanthorburn@gmail.com>
2022-09-25 01:16:20 -0400

    Prep CSS for JS carousel added

--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,22 @@
+// HAMBURGER MENU
+
+// Target hamburger menu symbol for animation and use desktop's nav bar with diff formatting to slide out
 const hamburger = document.querySelector('.hamburger');
 const fullMenu = document.querySelector('.desktop-nav-bar');
 
-// const slideOutMenu = (activate) => {
-//     activate.classList.add("active");
-//     activate.classList.remove("active");
-// }
 
+// Open menu and animate change of symbol
 hamburger.addEventListener('click', () => {
-    console.log(EventTarget);
+    // console.log(EventTarget);
     hamburger.classList.toggle('active');
     fullMenu.classList.toggle('active');
 })
-
+// Return to default if a link is clicked
+// I would have never figured out line 16's n => n without this video which heavily guided
+// my construction of this hamburger menu: https://youtu.be/flItyHiDm7E
 document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
     hamburger.classList.remove('active');
     fullMenu.classList.remove('active');
 }))
+
+// IMAGE CAROUSEL
